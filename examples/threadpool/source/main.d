bf8bb21b469be0b28ca21efe0b226f43890379b2
zoujiaqing <zoujiaqing@gmail.com>
2022-05-14 13:58:34 +0800

    Add example code for ThreadPool

---
+++ examples/threadpool/source/main.d
@@ -0,0 +1,29 @@
+
+import gear.util.ThreadPool;
+
+import std.stdio;
+
+import core.thread;
+import std.process;
+
+void main()
+{
+    auto pool = new ThreadPool(2);
+    Thread.sleep(1.seconds);
+    
+    for (int i = 0; i < 20; i++)
+    {
+        pool.Emplace(() {
+            writeln("Writeln i: ", i,", Thread id: ", thisThreadID());
+            Thread.sleep(1.seconds);
+        });
+    }
+
+    getchar();
+
+    writeln("Stopping....");
+    
+    pool.Stop();
+
+    writeln("All done.");
+}
